59bb2b5d41a86d78cb2b7351a1c9f2b9dea5c262
gautamkandpal <gkandpal33@gmail.com>
2024-10-13 22:37:34 +0530

    added react router functionality

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignIn() {
+  return <div>from SignIn</div>;
+}
+
+export default SignIn;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignUp() {
+  return <div>from signUp</div>;
+}
+
+export default SignUp;
