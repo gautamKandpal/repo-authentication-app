df48a24cc8ff15c49ca817ed1881269253a584bc
gautamkandpal <gkandpal33@gmail.com>
2024-10-13 23:40:56 +0530

    added headers section

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./component/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
